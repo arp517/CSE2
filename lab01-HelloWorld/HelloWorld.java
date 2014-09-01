0d8f9a8813383f6f218b29590f9e4256f18cb1bf
arp517 <arp517@lehigh.edu>
2014-09-01 15:32:10 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////
+//Anna Putelo
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it
+    System.out.println("################");
+    System.out.println("################");
+    System.out.println("#              #");
+    System.out.println("#  HelloWorld! #");
+    System.out.println("#              #");
+    System.out.println("################");
+    System.out.println("################");
+    System.out.println("testing");
+    //test/testtest
+    }
+}
