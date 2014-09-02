045d441ab06e32b6ad73e2184077235e6b6b39aa
arp517 <arp517@lehigh.edu>
2014-09-02 19:10:23 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////
+//Anna Putelo
+//Welcome Class Java Program
+//
+
+
+//define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| Welcome|");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\");
+    System.out.println(" |-A-R-P-5-1-7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v ");
+    System.out.println("This summer I dropped my phone \n on the driveway and it cracked");
+    //test/testtest
+    }
+}
